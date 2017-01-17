fb3141138c7ae3b645bfdae6c27041b970a245de
Juan Mora <raicerk@gmail.com>
2017-01-17 11:19:00 -0300

    Se cambia funcion de creacion de token

--- service.js
+++ service.js
@@ -3,10 +3,10 @@ var moment = require('moment');
 var config = require('./config');
 
 exports.createToken = function(user) {
-  var payload = {
-    sub: user._id,
-    iat: moment().unix(),
-    exp: moment().add(1, "minutes").unix(),
-  };
-  return jwt.encode(payload, config.TOKEN_SECRET);
+    var token = jwt.sign({
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(1, "minutes").unix()
+    }, config.TOKEN_SECRET);
+    return token;
 };
